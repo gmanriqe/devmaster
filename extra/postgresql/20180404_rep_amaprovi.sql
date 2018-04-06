374edc113d2d2ccaed22e6b58f02d28f95a0f8ae
gmanriqe <gmanriqe@gmail.com>
2018-04-05 21:51:44 -0500

    suministros encontrador AMAPROVI

--- extra/postgresql/20180404_rep_amaprovi.sql
+++ extra/postgresql/20180404_rep_amaprovi.sql
@@ -44,5 +44,59 @@ from nasca.ficha f
 inner join nasca.padroncg p
 on f.codsuministro = p.codsuministro
 --where p.urbanizacion='NASCA - APV AMAPROVI' and f.estado_conexion<>'' and
-where (p.sub_categoria like '%DOMESTICO%'<> f.categoria_agua like '%DOMESTICO%') and p.urbanizacion='NASCA - APV AMAPROVI'
+where (p.sub_categoria like '%DOMESTICO%' <> f.categoria_agua like '%DOMESTICO%') and p.urbanizacion='NASCA - APV AMAPROVI'
+order by f.manzana asc
+
+
+-- SUMINISTROS AUN NO TOCADOS -- TOTAL 49
+select 
+f.categoria_agua,
+p.sub_categoria,
+p.nombres as nombre_padron, 
+p.urbanizacion as nombre_habilitacion_padron, 
+p.codsuministro as suministro_padron, 
+f.codsuministro, 
+f.estado_conexion, 
+f.conexion, 
+f.codencuestador, 
+f.sector, 
+f.manzana, 
+f.lote, 
+f.tipo_habilitacion,
+f.nombre_habilitacion,
+f.tipo_via,
+f.nombre_via,
+f.referencias
+from nasca.ficha f
+inner join nasca.padroncg p
+on f.codsuministro = p.codsuministro
+where p.urbanizacion='NASCA - APV AMAPROVI' and f.estado_conexion is null  
+order by f.manzana asc
+
+-- SUMINISTROS ENCONTRADOS AMAPROVI
+select 
+p.codsuministro as suministro_padron, 
+p.nombres as nombre_padron, 
+--r.nombres ||' '||r.apellido_pat as nombres,
+f.tipo_via||' '||f.nombre_via ||' '|| f.num_municipal as nombre_via_ficha, -- ficha 
+f.tipo_habilitacion||' '||f.nombre_habilitacion as nombre_habilitacion_ficha, -- ficha
+
+f.categoria_agua as categoria_ficha, -- ficha
+p.sub_categoria as sub_categoria_padron, -- padron
+
+f.tipo_servicio as tipo_servicio_ficha, -- ficha
+p.tipo_servicio as tipo_servicio_padron, -- padron
+p.estado_servicio as estado_servicio_padron, --padron
+
+p.estado_agua as estado_agua_padron, -- padron
+f.estado_agua as estado_agua_ficha, -- ficha
+
+p.estado_desague as estado_desague_padron -- padron
+from nasca.ficha f
+inner join nasca.padroncg p
+on f.codsuministro = p.codsuministro
+--inner join nasca.responsable r
+--on f.id = r.idficha
+--where p.urbanizacion='NASCA - APV AMAPROVI' and f.estado_conexion is null --<>'' 
+where p.urbanizacion='NASCA - APV AMAPROVI' and estado_conexion <> '' --and r.tipo_resp='R'
 order by f.manzana asc
